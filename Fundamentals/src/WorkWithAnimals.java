28cf72df378b68b2991f2f13fb8b04a88f26d457
dontrellthedeveloper <dontrellknight@gmail.com>
2023-02-24 19:43:08 -0500

    oop fundamentals in java

---
+++ Fundamentals/src/WorkWithAnimals.java
@@ -0,0 +1,13 @@
+public class WorkWithAnimals {
+
+    public static void main(String[] args) {
+        Dog fido = new Dog();
+
+        fido.setName("Fido");
+        System.out.println(fido.getName());
+
+        fido.digHole();
+
+        fido.setWeight(-1);
+    }
+}
